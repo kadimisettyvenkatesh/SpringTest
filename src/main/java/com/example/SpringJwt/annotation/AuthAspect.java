3e31abce9d3135a4792afc4a5b530b0f4a48a66d
Kadimisetty <venkatesh@sails.solutions>
2019-05-21 14:12:29 +0530

    second security annotation

--- src/main/java/com/example/SpringJwt/annotation/AuthAspect.java
+++ src/main/java/com/example/SpringJwt/annotation/AuthAspect.java
@@ -1,9 +1,13 @@
 package com.example.SpringJwt.annotation;
 
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +18,11 @@ public class AuthAspect {
    AuthorizationImpl authBean;
    
    @Before("@annotation(Authorized) && args(request)")
-   public static void before(HttpServletRequest request){
+   public void before(JoinPoint joinPoint,HttpServletRequest request){
+	   MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		Method m =signature.getMethod();
+		Authorized customAnnotation = m.getAnnotation(Authorized.class);
+		System.out.println("Method Privelige:"+customAnnotation.enabled());
 	   
 	   System.out.println("insideeeeeeeeeeeeeeee&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
        if (!(request instanceof HttpServletRequest)) {
@@ -22,12 +30,17 @@ public class AuthAspect {
            new RuntimeException("request should be HttpServletRequesttype");
        }
        
-//       if(authBean.authorize(request.getHeader("Authorization"))){
-//           
-//       }else {
-//            throw new RuntimeException("auth error..!!!");
-//       }
+       if(authBean.authorize(request.getHeader("Authorization"))){
+           
+       }else {
+            throw new RuntimeException("auth error..!!!");
+       }
       
+       if(customAnnotation.enabled()){
+           
+       }else {
+            throw new RuntimeException("auth error..!!!");
+       }
    } 
    
 }
